1350c63a3c1fb268d4e0aa1872d82f3df2ecd1cc
Muthuraj <54229054+Muthuraj-web@users.noreply.github.com>
2025-04-25 18:22:33 +0530

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get("/com.apple.remotemanagement", (req, res) => {
   console.log('Headers:', req.headers);
 
-  const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://sathish-win1.csez.zohocorpin.com:9383/mdm/client/v1/enroll?encapiKey=a214c0a3&templateToken=98947936753d3e119601187bbfa1b8de"}]}'
+  const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://mdm.manageengine.in/mdm/client/v1/enroll?encapiKey=PHtE6r0MFODoiG588kMB7aPpFpbxM9wm%2Bu00L1JPuYlBAqRQTk1S%2Ft4tkTTk%2BB4uXaRCEvaZydlhsbzKuu%2FWJz3vYW8dXWqyqK3sx%2FVYSPOZ86260EFLs1k%3D&templateToken=ec433e208bacdd058fcd2f1f372cf81e"}]}'
   const inputStream = Buffer.from(responseText);
 
   const responseHeaders = {
