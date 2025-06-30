3e11b662dc1430d0c02f3b000c90b49cdbecfa70
Muthuraj <54229054+Muthuraj-web@users.noreply.github.com>
2025-06-30 21:03:21 +0530

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -2,33 +2,34 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/com.apple.remotemanagement", (req, res) => {
-  console.log('Headers:', req.headers);
+  	console.log('Headers:', req.headers);
 
-  const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://mdmqa-w11-7.csez.zohocorpin.com:9383/mdm/client/v1/enroll?encapiKey=883c7003&templateToken=810946b22335d4bc5328d13dc0a889c1"}]}'
-  const inputStream = Buffer.from(responseText);
+  	const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://mdmqa-w11-7.csez.zohocorpin.com:9383/mdm/client/v1/enroll?encapiKey=883c7003&templateToken=810946b22335d4bc5328d13dc0a889c1"}]}'
+  	const inputStream = Buffer.from(responseText);
 
-  const responseHeaders = {
-    "Content-Type": "application/json",
-    "Content-Length":inputStream.length
-  };
-  res.writeHead(200, responseHeaders);
+  	const responseHeaders = {
+    		"Content-Type": "application/json",
+    		"Content-Length":inputStream.length
+  	};
+  	res.writeHead(200, responseHeaders);
 
-  res.write(inputStream);
-  res.end();
+  	res.write(inputStream);
+  	res.end();
 });
 
 
 router.get("/redirectedDEPJSON", (req, res) => {
-	try {
-		const pssoJSON = '{"code":"com.apple.psso.required","description":"MDM Server requires the user to authenticate with Identity Provider - BY MEMDM","message":"The MDM server requires you to authenticate with your Identity Provider. Please follow the instructions provided by your organization to complete the authentication process - BY MEMDM","details":{"Package":{"ManifestURL":"https://platformsso-profile-url-824074885.development.catalystserverless.com/server/Platform-SSO-Profile/manifest"},"ProfileURL":"https://platformsso-profile-url-824074885.development.catalystserverless.com/server/Platform-SSO-Profile/profile","AuthURL":"https://platformsso-profile-url-824074885.development.catalystserverless.com/server/Platform-SSO-Profile/auth"}}';
-		const inputStream = Buffer.from(pssoJSON);
-		const responseHeaders = {"Content-Type": "application/json"};
+	const pssoJSON = '{"code":"com.apple.psso.required","description":"MDM Server requires the user to authenticate with Identity Provider - BY MEMDM","message":"The MDM server requires you to authenticate with your Identity Provider. Please follow the instructions provided by your organization to complete the authentication process - BY MEMDM","details":{"Package":{"ManifestURL":"https://platformsso-profile-url-824074885.development.catalystserverless.com/server/Platform-SSO-Profile/manifest"},"ProfileURL":"https://platformsso-profile-url-824074885.development.catalystserverless.com/server/Platform-SSO-Profile/profile","AuthURL":"https://platformsso-profile-url-824074885.development.catalystserverless.com/server/Platform-SSO-Profile/auth"}}';
+	const inputStream = Buffer.from(pssoJSON);
+	
+	const responseHeaders = {
+		"Content-Type": "application/json",
+		"Content-Length":inputStream.length
+	};
+	
   	res.writeHead(403, responseHeaders);
-		res.write(inputStream);
-		res.end();
-	} catch (err) {
-		console.error('Error reading file:', err);
-	}
+	res.write(inputStream);
+	res.end();
 })
 
 module.exports = router;
